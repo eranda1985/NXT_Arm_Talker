d84ae16b0f0ede25ab663a638117e3d40699c9b7
eranda <eranda@eranda-desktop.(none)>
2014-01-16 13:19:16 +0800

    The ROS node which talks to the arm controller in laptop

---
+++ build/CMakeFiles/armtalker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/armtalker.dir/src/armtalker.o"
+  "../bin/armtalker.pdb"
+  "../bin/armtalker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/armtalker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
